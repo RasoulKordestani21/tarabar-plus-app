76cab09f4299b5e4ceb588a03779c096f599180d
Rasoul <rasoulkordestani12@gmail.com>
2025-01-02 22:09:10 +0330

    feat: config tailwind and twrnc for project

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    colors: {
+      primary: "#0055AA",
+      secondary: "#FFAA00",
+      background: "#003366",
+      card: "#999999",
+      text: "#FFFFFF",
+      border: "#fdfe33",
+      notification: "#33fefe"
+    },
+    fontFamily: {
+      vazir: ["Vazir-Regular", "sans-serif"]
+    },
+    screens: {
+      sm: "380px",
+      md: "420px",
+      lg: "680px",
+      // or maybe name them after devices for `tablet:flex-row`
+      tablet: "1024px"
+    }
+  }
+};
